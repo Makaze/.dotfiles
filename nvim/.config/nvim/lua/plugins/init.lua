0b3b7224c9ad96b3b0198cdeef02ee831bfad2e5
Makaze <christopherslane.work@gmail.com>
2024-03-05 18:00:49 -0500

    Reset

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -471,20 +471,20 @@ local default_plugins = {
   },
 
   -- file managing , picker etc
-  -- {
-  --   "nvim-tree/nvim-tree.lua",
-  --   cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-  --   init = function()
-  --     require("core.utils").load_mappings "nvimtree"
-  --   end,
-  --   opts = function()
-  --     return require "plugins.configs.nvimtree"
-  --   end,
-  --   config = function(_, opts)
-  --     dofile(vim.g.base46_cache .. "nvimtree")
-  --     require("nvim-tree").setup(opts)
-  --   end,
-  -- },
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    init = function()
+      require("core.utils").load_mappings "nvimtree"
+    end,
+    opts = function()
+      return require "plugins.configs.nvimtree"
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "nvimtree")
+      require("nvim-tree").setup(opts)
+    end,
+  },
 
   {
     "nvim-telescope/telescope.nvim",

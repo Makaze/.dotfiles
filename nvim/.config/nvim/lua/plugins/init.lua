2f391666d35aef80c2ac92aac911cb3a01237207
Makaze <christopherslane.work@gmail.com>
2024-04-24 12:21:12 -0400

    Add img-clip

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1701,6 +1701,19 @@ local default_plugins = {
     config = true,
     event = "VeryLazy",
   },
+
+  {
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add options here
+      -- or leave it empty to use the default settings
+    },
+    keys = {
+      -- suggested keymap
+      { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    },
+  },
 }
 
 local config = require("core.utils").load_config()

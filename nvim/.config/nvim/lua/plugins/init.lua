194f8442ecee68135639d7df5e01c2794b889ba2
Makaze <christopherslane.work@gmail.com>
2024-08-01 19:19:01 -0400

    Remove autopairs

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -440,23 +440,23 @@ local default_plugins = {
         end,
       },
 
-      -- autopairing of (){}[] etc
-      {
-        "windwp/nvim-autopairs",
-        opts = {
-          fast_wrap = {},
-          disable_filetype = { "TelescopePrompt", "vim" },
-        },
-        config = function(_, opts)
-          require("nvim-autopairs").setup(opts)
-          require("nvim-autopairs").remove_rule('"')
-          require("nvim-autopairs").remove_rule("'")
-
-          -- setup cmp for autopairs
-          local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end,
-      },
+      -- -- autopairing of (){}[] etc
+      -- {
+      --   "windwp/nvim-autopairs",
+      --   opts = {
+      --     fast_wrap = {},
+      --     disable_filetype = { "TelescopePrompt", "vim" },
+      --   },
+      --   config = function(_, opts)
+      --     require("nvim-autopairs").setup(opts)
+      --     require("nvim-autopairs").remove_rule('"')
+      --     require("nvim-autopairs").remove_rule("'")
+      --
+      --     -- setup cmp for autopairs
+      --     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      --     require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      --   end,
+      -- },
 
       -- cmp sources plugins
       {
